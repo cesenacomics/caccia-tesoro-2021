60c76f3fbbc8250a6e2a66f08cea0853fe04ec92
mondoboia <giuliarenda94@gmail.com>
2021-07-23 10:14:56 +0200

    pw change

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function myFunction() {
     let psw = document.getElementById("pwd").value;
 
-    if (psw === '24HSY5ISAZ') {
+    if (psw === '24IEY5ISAZ') {
         window.open("landing.html");
     }  else {
         alert('Password sbagliata, riprova.')
